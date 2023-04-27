14fd36ecbb6f84d872da4a47c5e3809da93b53b8
BEATRIZBUFFON <118696360+BEATRIZBUFFON@users.noreply.github.com>
2023-04-27 16:10:16 -0300

    Update Exerc_03.py

--- Exerc_03.py
+++ Exerc_03.py
@@ -1,11 +1,9 @@
 a = float(input("Digite a nota do aluno A: "))
-a = float(input("Digite a nota do aluno B: "))
-a = float(input("Digite a nota do aluno C: "))
+b = float(input("Digite a nota do aluno B: "))
+c = float(input("Digite a nota do aluno C: "))
 media = (a+b+c)/3
 media_pond = (a*2 + b*3 + c*5)/ (2+3+5)
 media_pond = round(float(media_pond), 1)
 media = round(float(media), 1)
 print(f'MÉDIA PONDERADA = {media_pond}')
 print(f'MÉDIA = {media}')
-print(f'A média dos alunos é: {media}.')
-print(f'A média ponderada dos alunos é: {media_pond}')
