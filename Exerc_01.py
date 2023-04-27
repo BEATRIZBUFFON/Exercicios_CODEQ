3964e7270973e6d0ce27e60ef5376158bc7aa942
BEATRIZBUFFON <118696360+BEATRIZBUFFON@users.noreply.github.com>
2023-04-27 16:07:50 -0300

    Update Exerc_01.py

--- Exerc_01.py
+++ Exerc_01.py
@@ -1,8 +1,4 @@
 a = float(input("Digite um valor inteiro: "))
 b = float(input("Digite um valor inteiro: "))
-a = 5
-b = 6
 x = a + b
 print(f'X = {x}')
-print(f'A entrada contém dois valores inteiros, os quais são {a} e {b}.')
-print(f'O somatório deles resulta em: {x}')
